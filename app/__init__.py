b8fe36079d5e76703163cae7854d691e38ca6be1
Omar Amgad <omaramgad257@gmail.com>
2025-12-06 19:53:59 +0200

    Initialize database and register blueprints
    Added database initialization and registered blueprints for authentication and main routes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,20 @@
 from flask import Flask
-# We will import config from the outer folder
-# Note: We need to modify sys path or just import Config if it's in root
-# Standard way for this structure:
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    from . import db
+    db.init_app(app)
 
-    # We will add database and routes later
-    @app.route('/')
-    def hello():
-        return "Academia Connect is Running!"
+    from .controllers import auth_bp, main_bp
+    
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
 
     return app
